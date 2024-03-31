a6654b05f046e3cd3690461184b816fe3ddba93f
hk1996yhx <50992362+hk1996yhx@users.noreply.github.com>
2024-04-01 01:03:17 +0800

    Update 安装.bat

--- 安装.bat
+++ 安装.bat
@@ -12,12 +12,4 @@ if %errorlevel%==0 (
 echo Starting Docker build process...
 docker build -t skynet_docker .
 
-echo Running Docker container...
-docker run -d --name service-DB -p 8000:8000 -p 9000:9000 -v "%CD%/src:/yhx/src" skynet_docker ../skynet/skynet ./etc/config.nodedb
-
-if %errorlevel%==0 (
-    echo Docker container started successfully.
-) else (
-    echo Docker container failed to start.
-)
 pause
