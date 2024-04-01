1ecc8cf87df1868e3f458a3a6d8f23512693a48f
hk1996yhx <50992362+hk1996yhx@users.noreply.github.com>
2024-04-01 21:35:24 +0800

    Update 安装.bat

--- 安装.bat
+++ 安装.bat
@@ -1,15 +1,12 @@
 @echo off
 echo Checking if the container 'service-DB' already exists...
 docker container ls -a | findstr "service-DB" >nul 2>&1
-
 if %errorlevel%==0 (
     echo Stopping existing container...
     docker stop service-DB
     echo Removing existing container...
     docker rm service-DB
 )
-
 echo Starting Docker build process...
 docker build -t skynet_docker .
-
 pause
